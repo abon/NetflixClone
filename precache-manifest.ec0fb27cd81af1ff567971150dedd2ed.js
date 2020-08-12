df5036d715b3455f35709c49ae8213035407e5d8
Abdulloh <formygithubacc@gmail.com>
2020-08-12 15:14:00 +0500

    Updates

---
+++ precache-manifest.ec0fb27cd81af1ff567971150dedd2ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0d110ccc899dd83321c8cd8e1f6ae5",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "24c3fa6b2391c520fe79",
+    "url": "/netflix-clone/static/css/main.1151a965.chunk.css"
+  },
+  {
+    "revision": "6a2520800805b30ef35c",
+    "url": "/netflix-clone/static/js/2.4dd7607e.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.4dd7607e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c3fa6b2391c520fe79",
+    "url": "/netflix-clone/static/js/main.c4849c22.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
